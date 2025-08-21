a033264d561347222187db8d810826e2875ef089
devsafix <devsafix@gmail.com>
2025-08-22 00:19:33 +0600

    create auth slice for manage cookies and user info

---
+++ src/redux/features/auth/auth.slice.ts
@@ -0,0 +1,49 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  role: "rider" | "driver" | "admin";
+}
+
+// Define the shape of the authentication state.
+interface IAuthState {
+  user: IUser | null;
+  token: string | null;
+}
+
+const initialState: IAuthState = {
+  user: null,
+  token: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (
+      state,
+      action: PayloadAction<{ user: IUser; token: string }>
+    ) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { setCredentials, logout } = authSlice.actions;
+
+export default authSlice.reducer;
+
+// Selector to easily get the current user's data from the state.
+export const selectCurrentUser = (state: { auth: IAuthState }) =>
+  state.auth.user;
+
+// Selector to get the current user's token.
+export const selectCurrentToken = (state: { auth: IAuthState }) =>
+  state.auth.token;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { baseApi } from "./baseApi";
+import authReducer from "../redux/features/auth/auth.slice";
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
@@ -12,7 +14,5 @@ export const store = configureStore({
 
 setupListeners(store.dispatch);
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
