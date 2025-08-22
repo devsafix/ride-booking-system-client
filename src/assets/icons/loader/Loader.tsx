c2709d3d5f06d187ca47f50693dc4167414fc632
devsafix <devsafix@gmail.com>
2025-08-22 14:39:54 +0600

    loader updated

--- src/assets/icons/loader/Loader.tsx
+++ src/assets/icons/loader/Loader.tsx
@@ -1,26 +1,5 @@
 import "./loader.css";
 
 export default function Loader() {
-  return (
-    <div>
-      <div className="loader">
-        <span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
-        <div className="base">
-          <span></span>
-          <div className="face"></div>
-        </div>
-      </div>
-      <div className="longfazers">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
-  );
+  return <div className="loader"></div>;
 }
