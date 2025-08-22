6c49370856f494eb68461a302a5639833d9c8581
devsafix <devsafix@gmail.com>
2025-08-22 23:26:29 +0600

    api endpoint created for get ride history

--- src/redux/features/ride/ride.api.ts
+++ src/redux/features/ride/ride.api.ts
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { baseApi } from "@/redux/baseApi";
+import type { IResponse } from "@/types";
 
 export const rideApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
@@ -10,7 +12,26 @@ export const rideApi = baseApi.injectEndpoints({
       }),
       invalidatesTags: ["RIDER"],
     }),
+    getMyRides: build.query<
+      IResponse<null>,
+      {
+        page?: number;
+        limit?: number;
+        status?: string;
+        minFare?: number;
+        maxFare?: number;
+        startDate?: string;
+        endDate?: string;
+      }
+    >({
+      query: (params: any) => ({
+        url: "/rides/my",
+        method: "GET",
+        params,
+      }),
+      providesTags: ["RIDER"],
+    }),
   }),
 });
 
-export const { useRequestRideMutation } = rideApi;
+export const { useRequestRideMutation, useGetMyRidesQuery } = rideApi;
