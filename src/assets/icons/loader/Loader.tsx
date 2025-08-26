d90b1d90af44bd16315d3c4f6a4e75ac1f0d777c
devsafix <devsafix@gmail.com>
2025-08-26 16:23:45 +0600

    loader added

--- src/assets/icons/loader/Loader.tsx
+++ src/assets/icons/loader/Loader.tsx
@@ -1,5 +1,5 @@
 import "./loader.css";
 
 export default function Loader() {
-  return <div className="loader"></div>;
+  return <div className="loader min-h-[500px] flex items-center justify-between mx-auto"></div>;
 }
