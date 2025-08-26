4eabacd40b9063e8fe1eae3ee23ccd72b05214f4
devsafix <devsafix@gmail.com>
2025-08-26 11:16:55 +0600

    navbar corrected and show navlinks for mobile

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -28,6 +28,7 @@ import Logo from "@/assets/icons/logo";
 import userIcon from "../../assets/images/user-icon.webp";
 import { ModeToggle } from "./ModeToggler";
 import { baseApi } from "@/redux/baseApi";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 interface NavLink {
   href: string;
@@ -99,7 +100,55 @@ export default function Navbar() {
   return (
     <header className="border-b py-2 px-4 md:px-6 sticky top-0 z-50 backdrop-blur-xl">
       <div className="flex h-16 max-w-6xl mx-auto items-center justify-between gap-4">
-        {/* Left side: Logo */}
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              className="group size-10 md:hidden"
+              variant="ghost"
+              size="icon"
+            >
+              <svg
+                className="pointer-events-none"
+                width={16}
+                height={16}
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M4 12L20 12"
+                  className="origin-center -translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]"
+                />
+                <path
+                  d="M4 12H20"
+                  className="origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45"
+                />
+                <path
+                  d="M4 12H20"
+                  className="origin-center translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]"
+                />
+              </svg>
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent align="start" className="w-36 p-1 md:hidden">
+            <NavigationMenu className="max-w-none *:w-full">
+              <NavigationMenuList className="flex-col items-start gap-0 md:gap-2">
+                {currentLinks?.map((link, index) => (
+                  <NavigationMenuItem key={index} className="w-full">
+                    <NavigationMenuLink asChild className="py-1.5">
+                      <Link to={link.href}>{link.label}</Link>
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+                ))}
+              </NavigationMenuList>
+            </NavigationMenu>
+          </PopoverContent>
+        </Popover>
+        {/* main nav: Logo */}
         <div className="flex items-center gap-5">
           <Link to="/" className="text-primary hover:text-primary/90">
             <Logo />
